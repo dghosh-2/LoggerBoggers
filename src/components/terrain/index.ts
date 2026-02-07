d4a4d9850662261f01775fd4c0be8b2033545fe6
Krish Mody <krishmody@Krishs-MacBook-Pro.local>
2026-02-07 05:16:48 -0500

    Merge modifications from stash after pull: Resolve conflicts and restore terrain features

--- src/components/terrain/index.ts
+++
@@ -1,8 +0,0 @@
-export { TerrainModal } from "./TerrainModal";
-export { StackedTerrainView } from "./StackedTerrainView";
-export { FinancialTerrain } from "./FinancialTerrain";
-export { TerrainParticles } from "./TerrainParticles";
-export { EventMarkers } from "./EventMarkers";
-export { TimeScrubber } from "./TimeScrubber";
-export { CategoryToggles } from "./CategoryToggles";
-export { StatsHUD } from "./StatsHUD";
