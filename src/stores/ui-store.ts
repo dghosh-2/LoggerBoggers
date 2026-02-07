87cd09c59bb71be65f412c1b98ec2ede23fbc38c
Abhinav Akkiraju <43082610+AbhinavAkkiraju@users.noreply.github.com>
2026-02-07 01:20:09 -0500

    hid chatbot on hero page

--- src/stores/ui-store.ts
+++ src/stores/ui-store.ts
@@ -3,9 +3,13 @@ import { create } from "zustand";
 interface UIState {
   navbarHidden: boolean;
   setNavbarHidden: (hidden: boolean) => void;
+  chatHidden: boolean;
+  setChatHidden: (hidden: boolean) => void;
 }
 
 export const useUIStore = create<UIState>((set) => ({
   navbarHidden: false,
   setNavbarHidden: (hidden) => set({ navbarHidden: hidden }),
+  chatHidden: false,
+  setChatHidden: (hidden) => set({ chatHidden: hidden }),
 }));
