c8ef76eb77cb70cf9dfa143fb08d8e2bfc2028a4
Abhinav Akkiraju <43082610+AbhinavAkkiraju@users.noreply.github.com>
2026-02-06 21:23:30 -0500

    cmu customizations
    plaid background

--- src/stores/theme-store.ts
+++ src/stores/theme-store.ts
@@ -9,7 +9,7 @@ interface ThemeState {
 }
 
 export const useThemeStore = create<ThemeState>((set, get) => ({
-  theme: "light",
+  theme: "dark",
   setTheme: (theme) => set({ theme }),
   toggleTheme: () => set({ theme: get().theme === "dark" ? "light" : "dark" }),
 }));
